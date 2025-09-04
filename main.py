497b7547af1b0d5f1eb89de6284633c6ec69dfba
fplradar <sandjiry@gmail.com>
2025-09-05 00:29:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
-# Script de test : il crée/écrase un fichier résumé.md avec l'heure actuelle
 now = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S UTC")
 
-with open("résumé.md", "w", encoding="utf-8") as f:
+with open("resume.md", "w", encoding="utf-8") as f:
     f.write(f"# Rapport automatique\n\nDernière exécution : {now}\n")
 
-print("Fichier résumé.md généré.")
+print("Fichier resume.md généré.")
+
